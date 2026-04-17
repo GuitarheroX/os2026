1104885fb287fe6e03ee2aa6ca6dea7e0b9845f3
tracer-nju <tracer@nju.edu.cn>
2026-04-17 20:25:46 +0800

    Linux Xiao 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:25:46 up  1:31,  1 user,  load average: 0.00, 0.04, 0.02

---
+++ .shadow/labyrinth/labyrinth.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <testkit.h>
+#include <unistd.h>
+#include <getopt.h>
+#include "labyrinth.h"
+
+int main(int argc, char *argv[]) {
+    const char *short_options = "u";
+    const struct option long_opts[] = {
+        {"usage", no_argument, 0, 'u'},
+        {0, 0, 0, 0}
+    };
+
+    int c;
+
+    while ((c = getopt_long(argc, argv, short_options) != -1)){
+        switch (c) {
+            case 'u':
+                printUsage();
+                return 0;
+            default:
+                abort();
+        }
+    }
+    return 0;
+}
+
+void printUsage() {
+    printf("Usage:\n");
+    printf("  labyrinth --map map.txt --player id\n");
+    printf("  labyrinth -m map.txt -p id\n");
+    printf("  labyrinth --map map.txt --player id --move direction\n");
+    printf("  labyrinth --version\n");
+}
+
+bool isValidPlayer(char playerId) {
+    if (c >= '0' && c <= '9'){
+        return true;
+    }
+    return false;
+}
+
+bool loadMap(Labyrinth *labyrinth, const char *filename) {
+    // TODO: Implement this function
+    return false;
+}
+
+Position findPlayer(Labyrinth *labyrinth, char playerId) {
+    // TODO: Implement this function
+    Position pos = {-1, -1};
+    return pos;
+}
+
+Position findFirstEmptySpace(Labyrinth *labyrinth) {
+    // TODO: Implement this function
+    Position pos = {-1, -1};
+    return pos;
+}
+
+bool isEmptySpace(Labyrinth *labyrinth, int row, int col) {
+    // TODO: Implement this function
+    return false;
+}
+
+bool movePlayer(Labyrinth *labyrinth, char playerId, const char *direction) {
+    // TODO: Implement this function
+    return false;
+}
+
+bool saveMap(Labyrinth *labyrinth, const char *filename) {
+    // TODO: Implement this function
+    return false;
+}
+
+// Check if all empty spaces are connected using DFS
+void dfs(Labyrinth *labyrinth, int row, int col, bool visited[MAX_ROWS][MAX_COLS]) {
+    // TODO: Implement this function
+}
+
+bool isConnected(Labyrinth *labyrinth) {
+    // TODO: Implement this function
+    return false;
+}
