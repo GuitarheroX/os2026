f35e82eddee7f74d19852cd8a7cf5b0da7a20cd1
tracer-nju <tracer@nju.edu.cn>
2026-04-18 15:39:57 +0800

    Linux Xiao 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:39:57 up 36 min,  1 user,  load average: 0.00, 0.00, 0.00

--- .shadow/labyrinth/labyrinth.c
+++ .shadow/labyrinth/labyrinth.c
@@ -36,6 +36,7 @@ int main(int argc, char *argv[]) {
 
 
     loadMap(&labyrinth, "./maps/map.txt");
+    printf("size is %s", sizeof(labyrinth.map));
     printf("%s", labyrinth.map[0][0]);
     printf("rows %d\n", labyrinth.rows);
     printf("cols %d\n", labyrinth.cols);
