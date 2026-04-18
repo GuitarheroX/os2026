b575a5842f2dd051bb3faa452e7c81e96ba3242d
tracer-nju <tracer@nju.edu.cn>
2026-04-18 15:59:16 +0800

    Linux Xiao 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:59:16 up 55 min,  1 user,  load average: 0.08, 0.02, 0.01

--- .shadow/labyrinth/labyrinth.c
+++ .shadow/labyrinth/labyrinth.c
@@ -58,6 +58,7 @@ int main(int argc, char *argv[]) {
     }
     rows = r;
     fclose(file);
+    printf("1");
     for (int i = 0; i < rows; i++){
         for (int j = 0; j < cols; j++){
             //printf("111");
