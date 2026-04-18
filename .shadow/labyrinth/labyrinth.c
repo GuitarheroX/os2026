6460a05b57b1cf554f830a336b447c63de9a0e8a
tracer-nju <tracer@nju.edu.cn>
2026-04-18 15:34:39 +0800

    Linux Xiao 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:34:39 up 31 min,  1 user,  load average: 0.00, 0.00, 0.00

--- .shadow/labyrinth/labyrinth.c
+++ .shadow/labyrinth/labyrinth.c
@@ -37,7 +37,7 @@ int main(int argc, char *argv[]) {
     printf("rows %d\n", labyrinth.rows);
     printf("cols %d\n", labyrinth.cols);
 
-    printf("%s", labyrinth.map[0][0])
+    printf("%s", labyrinth.map[0][0]);
     for (int i = 0; i < labyrinth.rows; i++){
         for (int j = 0; j < labyrinth.cols; j++){
             //printf("111");
