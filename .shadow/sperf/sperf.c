a4bf9bc70317a67b9967776d35f86551a12672b8
tracer-nju <tracer@nju.edu.cn>
2026-05-27 12:11:58 +0800

    Linux Xiao 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:11:58 up  2:21,  1 user,  load average: 0.00, 0.00, 0.00

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -38,7 +38,7 @@ char *find_in_path(const char *file) {
     char *result = NULL;
     while(dir) {
         char fullpath[1024];
-        snprintf(fullpath, sizeof(fullpath), "%s%s", dir, file);
+        snprintf(fullpath, sizeof(fullpath), "%s/%s", dir, file);
         // debug
         printf("Now in %s\n", fullpath);
         if (access(fullpath, F_OK) == 0) {
