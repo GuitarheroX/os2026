2c80a5e3e70c364a2909d8874173751f2bb48d99
tracer-nju <tracer@nju.edu.cn>
2026-05-31 11:14:06 +0800

    Linux Xiao 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:14:06 up 36 min,  1 user,  load average: 0.00, 0.00, 0.00

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-#include <math.h>
 #include <time.h>
 #include <fcntl.h>
 #include <stdio.h>
@@ -67,7 +66,7 @@ void print_top_syscalls(syscall_stats *s_stats, int n) {
     qsort(s_stats->stats, s_stats->count, sizeof(syscall_stat), cmp);
 
     for (int i = 0; i < n; i++) {
-        printf("%s (%d%%)\n", s_stats->stats[i].name, (int)round(s_stats->stats[i].time / s_stats->total_time));
+        printf("%s (%d%%)\n", s_stats->stats[i].name, (int)(s_stats->stats[i].time / s_stats->total_time));
     }
 }
 
