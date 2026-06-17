bb7d825d53a4583d76c3922f5f3a0fe7536b3400
tracer-nju <tracer@nju.edu.cn>
2026-06-17 11:54:11 +0800

    Linux Xiao 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:54:11 up  1:55,  1 user,  load average: 0.00, 0.00, 0.00

--- .shadow/crepl/crepl.c
+++ .shadow/crepl/crepl.c
@@ -70,7 +70,7 @@ bool evaluate_expression(const char* expression, int* result) {
         waitpid(pid, &status, 0);
 
         void *handle = dlopen(so);
-        void (*func)() = dlsym(handle, func_name);
+        int (*func)() = dlsym(handle, func_name);
         *result = func();
         return true;
     }
